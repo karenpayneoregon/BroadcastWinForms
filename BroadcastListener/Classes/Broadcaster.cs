00c9ef8392c7622cf69e288a05c06f82b80d33c4
Karen Payne <paynekaren@comcast.net>
2019-12-29 12:08:21 -0800

    Code formatting

--- BroadcastListener/Classes/Broadcaster.cs
+++ BroadcastListener/Classes/Broadcaster.cs
@@ -8,40 +8,41 @@ namespace BroadcastListener.Classes
     public class Broadcaster
     {
         private readonly Collection<IMessageListener1> _listeners = 
-            new Collection<IMessageListener1>(); 
+            new Collection<IMessageListener1>();
 
         /// <summary>
         /// Send message
         /// </summary>
-        /// <param name="Message">Message</param>
+        /// <param name="message">Message</param>
+        /// <param name="sender"></param>
         /// <remarks></remarks>
         [DebuggerStepThrough()]
-        public void Broadcast(string Message, Form sender)
+        public void Broadcast(string message, Form sender)
         {
             foreach (IMessageListener1 listener in _listeners)
             {
-                listener.OnListen(Message, sender);
+                listener.OnListen(message, sender);
             }
         }
        
         /// <summary>
         /// Add a Listener to the Collection of Listeners
         /// </summary>
-        /// <param name="Listener"></param>
-        public void AddListener(IMessageListener1 Listener)
+        /// <param name="listener"></param>
+        public void AddListener(IMessageListener1 listener)
         {
-            _listeners.Add(Listener);
+            _listeners.Add(listener);
         }
         /// <summary>
         /// Remove a Listener from the collection
         /// </summary>
-        /// <param name="Listener"></param>
-        public void RemoveListener(IMessageListener1 Listener)
+        /// <param name="listener"></param>
+        public void RemoveListener(IMessageListener1 listener)
         {
 
             for (int index = 0; index < _listeners.Count; index++)
             {
-                if ( _listeners[index].Equals(Listener) )
+                if ( _listeners[index].Equals(listener) )
                 {
                     _listeners.Remove(_listeners[index]);
                 }
